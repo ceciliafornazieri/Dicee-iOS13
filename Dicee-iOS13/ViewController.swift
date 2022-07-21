61c568f27913643a7563325d3e021e0cfcc9e5a9
ceciliafornazieri <fornaziericecilia@gmail.com>
2022-07-21 15:36:27 -0300

    nao sei

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+    @IBOutlet weak var diceOne: UIImageView!
+    
+    @IBOutlet weak var diceTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+  
+    @IBAction func rollButtonPressed(_ sender: Any){
+        
+        let diceArray = [ UIImage (named: "DiceOne"),
+                           UIImage (named: "DiceTwo"),
+                           UIImage (named: "DiceThree"),
+                           UIImage (named: "DiceFour"),
+                           UIImage (named: "DiceFive"),
+                           UIImage (named: "DiceSix")]
+                         
+        diceOne.image = diceArray [Int.random(in: 0...5)]
+        
+        diceTwo.image = diceArray [Int.random(in: 0...5)]
+        
+        
+        // o dado da esquerda vai ser igual ao seu valor atual (que ta ali em cima) e vai aumentar em 1 toda vez que eu pressionar o botao roll
+        
+          // dadoEsquerdo = seu valor atual + 1
+//        leftDiceNumber = leftDiceNumber + 1
+        
+        // o dado da direita vai ser igual o seu valor atual ( que está ali em cima na variavel right dice number = 5 ( numero dele na posicao do array) e vai diminuir em Uma posicao toda vez que eu apertar o botao roll
+        
+        //dadoDireito = Seu valor atual menos 1
+//        rightDiceNumber = rightDiceNumber - 1
+        
+        // porém eu posso escrever isso de um jeito mais fácil
+        
+        //aqui ele já pega o vzlor atual e soma/diminui 1 sem eu ter que escrever aquela frase enorme
+        
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+        
+
     }
 
-
+    
 }
 
